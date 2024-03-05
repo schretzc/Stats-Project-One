343491c582055f403b639ed8f66785e902a7eefc
chris11215 <schretzc@go.stockton.edu>
2024-03-05 15:52:56 -0500

    coinflip for turn

--- Pokemon Stuff + Monte/Pokemon Game/PokemonCardGame.java
+++ Pokemon Stuff + Monte/Pokemon Game/PokemonCardGame.java
@@ -26,7 +26,6 @@ public class PokemonCardGame {
         name2 = scan.nextLine();
         playerOne = new Player(name1);
         playerTwo = new Player(name2);
-        setupGame();
     }
 
     /**
@@ -112,11 +111,50 @@ public class PokemonCardGame {
     }
 
     /**
+     * flips coin
+     * @return returns string of coin flip
+     */
+    public String flipCoin(){
+        int coin = (int)(Math.random() * 2);
+        if (coin == 0){
+            return "heads";
+        }
+        else{
+            return "tails";
+        }
+    }
+
+    public boolean pickCoin(){
+        System.out.println("Player One, pick heads or tails: ");
+        String choice = scan.nextLine();
+        //String coin = flipCoin();
+        
+        if(!choice.equals("heads") && !choice.equals("tails")){
+            System.out.println("Invalid input, try again");
+            return pickCoin();
+        }
+
+        String coin = flipCoin();
+
+        if (choice.equals(coin)){
+            System.out.println("The coin landed on " + coin);
+            System.out.println("Player One goes first");
+            return true;
+        }
+        else{
+            System.out.println("The coin landed on " + coin);
+            System.out.println("Player Two goes first");
+            return false;
+        }
+    }
+    
+    /**
      * sets the game up
      * draws hands for both players
      * draws prize piles for both players
      */
     public void setupGame(){
+
         playerOne.drawHand();
         playerTwo.drawHand();
         playerOne.drawPrizePile();
@@ -130,7 +168,10 @@ public class PokemonCardGame {
      * and checks the winning conditions to break loop and crown the winner
      */
     public void runGame(){
+        setupGame();
+        boolean pick = pickCoin();
         while (playerOne.getPrizePile().size() > 0 || playerTwo.getPrizePile().size() > 0){
+            if (pick == true){
             System.out.println("Player One's Turn");
             printField(playerOne, playerTwo);
             playerOne.printHand();
@@ -148,6 +189,27 @@ public class PokemonCardGame {
             if (checkWinner() == true){
                 break;
             }
+        }
+        else {
+            System.out.println("Player Two's Turn");
+            printField(playerTwo, playerOne);
+            playerTwo.printHand();
+            playerTwo.turn(playerTwo, playerOne);
+            //checkWinner();
+            if (checkWinner() == true){
+                break;
+            }
+
+            System.out.println("Player One's Turn");
+            printField(playerOne, playerTwo);
+            playerOne.printHand();
+            playerOne.turn(playerOne, playerTwo);
+            //checkWinner();
+            if (checkWinner() == true){
+                break;
+            }
+
+        }
             
     }
 }
